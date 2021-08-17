362f16fc495cb8a63639bfa920bd9f529e9217a0
vyacheslav gorb <slav2712.2@gmail.com>
2021-08-17 23:35:17 +0300

    Replace session invalidation in logout command with removing user attribute

--- src/main/java/edu/gorb/musicstudio/controller/command/impl/LogoutCommand.java
+++ src/main/java/edu/gorb/musicstudio/controller/command/impl/LogoutCommand.java
@@ -3,13 +3,14 @@ package edu.gorb.musicstudio.controller.command.impl;
 import edu.gorb.musicstudio.controller.command.Command;
 import edu.gorb.musicstudio.controller.command.CommandResult;
 import edu.gorb.musicstudio.controller.command.PagePath;
+import edu.gorb.musicstudio.controller.command.SessionAttribute;
 
 import javax.servlet.http.HttpServletRequest;
 
 public class LogoutCommand implements Command {
     @Override
     public CommandResult execute(HttpServletRequest request) {
-        request.getSession().invalidate();
+        request.getSession().removeAttribute(SessionAttribute.USER);
         return new CommandResult(PagePath.HOME_PAGE_REDIRECT, CommandResult.RoutingType.REDIRECT);
     }
 }
